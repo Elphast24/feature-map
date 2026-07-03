34173b351642cde1dafb4f65606ef4a5f14262f9
Elphast24 <unrelinquish2020@gmail.com>
2026-07-03 20:41:43 +0000

    test update

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 	const disposable = vscode.commands.registerCommand('sbatlas.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from SBAtlas!');
+		vscode.window.showInformationMessage('Hello World from SBAtlas!, prepare for a new adventure!');
 	});
 
 	context.subscriptions.push(disposable);
